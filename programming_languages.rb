27ff46a8cae27d93552ace08580987181b37871e
patrickp246 <patrickp246@gmail.com>
2018-09-16 10:50:45 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |k,v|
     v.each do |l,w|
-      new_hash[l] = w
+      new_hash[l] ||= w
       new_hash[l][:style] ||= []
       new_hash[l][:style] << k
     end
